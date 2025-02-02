3f8b3f2381f0441a18d6f44ffdcd8327ac9541d2
kiarainfotech <161743345+kiarainfotech@users.noreply.github.com>
2025-02-02 19:35:39 +0000

    new web

---
+++ _next/static/chunks/app/(darkVersion)/projectdark/page-2b72bed91dcf4987.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7707,6888,440,8675,5063,5223,7901,3372,4568,7716,4933,4862,2943,3186,3918,4346,323,820,1434,2158,907],{6193:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[4609,2971,2117,1744],function(){return e(e.s=6193)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/(darkVersion)/projectdark/project-details-right-dark/page-c4cf82b077dc3982.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5223,6888,7707,440,8675,5063,7901,3372,4568,7716,4933,4862,2943,3186,3918,4346,323,820,1434,2158,907],{6193:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[4609,2971,2117,1744],function(){return e(e.s=6193)}),_N_E=e.O()}]);
