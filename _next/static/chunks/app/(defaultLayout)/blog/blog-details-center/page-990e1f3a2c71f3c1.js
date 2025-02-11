aab41bdc4f36daf9b57df90a075d0a5569ce813b
kiarainfotech <161743345+kiarainfotech@users.noreply.github.com>
2025-02-11 20:46:06 +0000

    updates

--- _next/static/chunks/app/(defaultLayout)/blog/blog-details-center/page-990e1f3a2c71f3c1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3372,440,8675,7901,7707,3918,5223,907,5063,4862,7716,4568,6888,2943,3186,4346,1434,4933,323,820,2158],{8971:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[4609,2971,2117,1744],function(){return e(e.s=8971)}),_N_E=e.O()}]);
