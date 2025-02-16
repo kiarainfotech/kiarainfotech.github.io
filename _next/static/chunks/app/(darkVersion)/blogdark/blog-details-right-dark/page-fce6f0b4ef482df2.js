bf1f0a6bed90bda6b2ad1df25d4ca25550653ca4
kiarainfotech <161743345+kiarainfotech@users.noreply.github.com>
2025-02-16 10:43:30 +0000

    updates

--- _next/static/chunks/app/(darkVersion)/blogdark/blog-details-right-dark/page-fce6f0b4ef482df2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8675,5063,907,6888,7901,3918,7707,5223,440,7716,3372,323,4346,1434,3186,4862,2943,820,2158,4933,4568],{8971:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[4609,2971,2117,1744],function(){return e(e.s=8971)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/(defaultLayout)/services/service-details/page-cc0acdc259b95bab.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[36],{},function(n){n.O(0,[609,653,971,117,744],function(){return n(n.s=8653)}),_N_E=n.O()}]);
