7884cb0fc1cca834fc8dfba1947af4f03f5055fe
kiarainfotech <161743345+kiarainfotech@users.noreply.github.com>
2025-03-09 21:37:33 +0000

    updates

--- _next/static/chunks/app/(defaultLayout)/industries/service-details/page-a85a3cada1ff1997.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[739],{},function(n){n.O(0,[609,521,971,117,744],function(){return n(n.s=5891)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/(defaultLayout)/services/(internal)/bookkeeping/page-7c06af62c210e046.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[23],{},function(n){n.O(0,[609,259,971,117,744],function(){return n(n.s=259)}),_N_E=n.O()}]);
